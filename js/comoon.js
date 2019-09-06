d230cf23d5920f55f50e8ff7bc5decd0992f56e4
tina <tinaxu@saas.top>
2019-09-06 17:10:23 +0800

    travel

---
+++ js/comoon.js
@@ -0,0 +1,11 @@
+/*
+* @Author: Marte
+* @Date:   2018-11-25 13:05:44
+* @Last Modified by:   Marte
+* @Last Modified time: 2018-11-25 15:12:49
+*/
+
+'use strict';
+function my$(id){
+    return document.getElementById(id);
+}
